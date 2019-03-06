a16af134a4fec29c39e493a92bb9e9f3d0866d8c
PacersCyc <452720855@qq.com>
2019-03-06 15:38:42 +0800

    client目录及路由配置

--- client/app.js
+++ client/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AppContainer } from 'react-hot-loader'; //eslint-disable-line
-import App from './App.jsx';
+import App from './views/App';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -12,7 +13,9 @@ const root = document.getElementById('root');
 const render = (Component) => {
   ReactDOM.hydrate(
     <AppContainer>
-      <Component />
+      <BrowserRouter>
+        <Component />
+      </BrowserRouter>
     </AppContainer>,
     root,
   )
@@ -21,8 +24,8 @@ const render = (Component) => {
 render(App);
 
 if (module.hot) {
-  module.hot.accept('./App.jsx', () => {
-    const NextApp = require('./App.jsx').default;  //eslint-disable-line
+  module.hot.accept('./views/App.jsx', () => {
+    const NextApp = require('./views/App.jsx').default;  //eslint-disable-line
     render(NextApp);
   })
 }
